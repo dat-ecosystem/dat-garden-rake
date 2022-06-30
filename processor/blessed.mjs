f1c3b33e0ec40503d87f7e3a20d3d7bfc419aa88
Martin Heidegger <martin.heidegger@gmail.com>
2022-07-01 08:47:40 +0900

    blessed: updated format to have a predictable URL key to look up

--- processor/blessed.mjs
+++ processor/blessed.mjs
@@ -1,4 +1,5 @@
-import { normalizeNPM, parseNpmUrl } from '../lib/npm.mjs'
+import { normalizeNPM, npmURL, parseNpmUrl } from '../lib/npm.mjs'
+import { createRepoTasks, githubRepoURL } from '../lib/repo.mjs'
 import { dependency } from './dependency.mjs'
 import { npmDependents } from './npm-dependents.mjs'
 import { repoDependents } from './repo-dependents.mjs'
@@ -6,23 +7,27 @@ import { repoDependents } from './repo-dependents.mjs'
 export const blessed = {
   type: 'blessed',
   async process (api, task) {
-    const { npm, repoURL } = task
-    if (npm) {
-      const url = await normalizeNPM(api, npm, task.version ?? '*')
+    const { blessed } = task
+    if (blessed.startsWith(npmURL)) {
+      const parsed = parseNpmUrl(blessed)
+      const url = await normalizeNPM(api, parsed.name, parsed.version ?? '*')
       const { name, version } = parseNpmUrl(url)
       return {
         batch: [
           ...await npmDependents.createTask(api, { name, version, depth: 0 }),
-          ...await dependency.createTask(api, { dependency: url })
+          ...await dependency.createTask(api, { dependency: url }),
+          { type: 'put', sublevel: api.meta, key: `blessed#${blessed}`, value: url }
         ]
       }
-    } else {
-      // TODO: This loads github dependents but not yet dependencies
+    }
+    if (blessed.startsWith(githubRepoURL)) {
       return {
         batch: [
+          { type: 'put', sublevel: api.blessed, key: `blessed#${blessed}`, value: blessed },
           // ...await repoDependents.createTask(api, { repoURL, depth: 0 })
         ]
       }
     }
+    throw new Error(`Unsupported blessed entry ${blessed}`)
   }
 }
